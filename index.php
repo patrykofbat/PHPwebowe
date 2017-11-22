aec55e3e437da6d5bc0812ae505803e098732adc
Patryk Papiór <patrykpapior1997@gmail.com>
2017-11-22 12:49:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+ 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Lab PHP</title>
+</head>
+<body>
+    <form action="action.php" method="post">
+        <input name="imie"/>
+        <input type="submit" value="Wyślij formularz" />
+    </form>
+	<?php echo 'Przykładowy tekst' ?>
+</body>
+</html>
