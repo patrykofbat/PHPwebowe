ba80239c50e5685172457213285612037d44ff3c
Patryk Papiór <patrykpapior1997@gmail.com>
2017-12-05 14:30:00 +0100

    asdsaddsaads

--- addwpis.php
+++ addwpis.php
@@ -2,33 +2,24 @@
 echo(file_get_contents("header.html"));
 require_once "classBlog.php"; 
 session_start();
-echo "<pre>";
-print_r($_POST);
 $date = getdate();
 $wpis = $_POST["wpis"];
 $explosion = explode("-", $_POST["date"]);
 $timeExp = explode(":", $_POST["time"]);
-print_r($explosion);
-print_r($timeExp);
 $nameOfBlog = $_SESSION["nazwa"];
 $blog = $_SESSION["$nameOfBlog"];
 $uniqueNumber = rand(10, 99);
 $nameOfFile = $explosion[0].$explosion[1].$explosion[2].$timeExp[0].$timeExp[1].$date["seconds"]."$uniqueNumber";
-echo $nameOfFile;
-echo "<pre/>";
-print_r($blog);
+
 
 ###########################################################
 if(strlen($wpis)>0){
     $blog->addEntry($wpis, $nameOfFile);
+    echo "Dodano wpis";
+    
 }
 if(isset($_FILES)){
     $blog->addAttachments($_FILES, $nameOfFile);
 }    
-    
-
-
-
-
 ?>
 <a href="menu.html">Powrót do menu</a>
